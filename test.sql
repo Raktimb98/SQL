fbfda79fd900d42b4ac50dd47fc551decd881e16
Raktim Biswas <raktimb181@gmail.com>
2024-06-22 09:45:25 +0530

    some practice

--- test.sql
+++ test.sql
@@ -1,1 +1,17 @@
-create database test;
+CREATE DATABASE xyz;
+USE xyz;
+CREATE TABLE testing(
+id INT PRIMARY KEY,
+name VARCHAR(51),
+city VARCHAR(21)
+);
+INSERT INTO testing
+(id,name,city)
+values
+(1,"Raktim Biswas","Ranaghat");
+SELECT * FROM testing;
+UPDATE testing
+SET name = "Roni Biswas"
+WHERE name = "Raktim Biswas";
+SET SQL_SAFE_UPDATES = 0;
+SELECT * FROM testing;
